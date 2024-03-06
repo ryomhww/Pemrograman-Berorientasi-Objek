1613626d8b386b5a4abf4580d36cfb2ef0ed62f5
ryomhww <114847378+ryomhww@users.noreply.github.com>
2024-03-06 14:44:24 +0700

    Update App.java

--- Looping/src/App.java
+++ Looping/src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        ////Saya semangat belajar java sebanyak 7 kali
     for (int i = 0; i < 7; i++){
         System.out.println("saya semangat belajar java");
     }
-    
+    //buatlah looping deret angka : 1 3 5 7 9 11
     int angka = 1;
     int beda = 2;
     for (int i = 1; i <7; i++){
@@ -14,13 +15,13 @@ public class App {
         
     }
     System.out.println(" ");
-    
+    //buatlah looping deret angka : 9 6 3 0 -3 -6
     for (int i = 9; i >= -6; i -= 3){
         
         System.out.print(i+" ");
     }
     System.out.println(" ");
-
+    //buatlah looping perkalian
     for (int i = 1; i <= 4; i++) {
         int hasil = 4 * i;
         System.out.println(4+"x"+i+"="+hasil);
